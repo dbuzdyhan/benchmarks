b8f2b13fc410b45c8839bd56128a62f48fa5c759
Dmytro Buzdyhan <d.buzdyhan@spd-ukraine.com>
2019-06-26 12:13:27 +0300

    added benchmark result

--- README.md
+++ README.md
@@ -1,1 +1,26 @@
 # benchmarks
+Run it in terminal 
+./gradlew clean jmh --no-daemon
+
+Memory 15.5 GiB
+Processor Intel® Core™ i7-7700HQ CPU @ 2.80GHz × 8 
+Graphic Intel® HD Graphics 630 (Kaby Lake GT2)
+
+Result "com.jsonparser.JsonParserTest.jackson":
+  37.638 ±(99.9%) 1.703 ms/op [Average]
+  (min, avg, max) = (34.452, 37.638, 42.982), stdev = 2.274
+  CI (99.9%): [35.935, 39.342] (assumes normal distribution)
+
+Result "com.jsonparser.JsonParserTest.gson":
+  38.994 ±(99.9%) 1.096 ms/op [Average]
+  (min, avg, max) = (37.363, 38.994, 42.087), stdev = 1.463
+  CI (99.9%): [37.898, 40.091] (assumes normal distribution)
+
+
+Benchmark               Mode  Cnt   Score   Error  Units
+JsonParserTest.gson     avgt   25  38.994 ± 1.096  ms/op
+JsonParserTest.jackson  avgt   25  37.638 ± 1.703  ms/op
+
+
+jmh group: 'com.fasterxml.jackson.core', name: 'jackson-databind', version: '2.9.9'
+jmh group: 'com.google.code.gson', name: 'gson', version: '2.8.5'
--- src/jmh/java/com/jsonparser/JsonParserTest.java
+++ src/jmh/java/com/jsonparser/JsonParserTest.java
@@ -2,16 +2,14 @@ package com.jsonparser;
 
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.gson.Gson;
 import org.openjdk.jmh.annotations.Benchmark;
 import org.openjdk.jmh.annotations.BenchmarkMode;
-import org.openjdk.jmh.annotations.Fork;
-import org.openjdk.jmh.annotations.Measurement;
 import org.openjdk.jmh.annotations.Mode;
 import org.openjdk.jmh.annotations.OutputTimeUnit;
 import org.openjdk.jmh.annotations.Scope;
 import org.openjdk.jmh.annotations.Setup;
 import org.openjdk.jmh.annotations.State;
-import org.openjdk.jmh.annotations.Warmup;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -24,26 +22,33 @@ import java.util.stream.Collectors;
 @State(Scope.Benchmark)
 @BenchmarkMode(Mode.AverageTime)
 @OutputTimeUnit(value = TimeUnit.MILLISECONDS)
-public class JacksonJsonParserTest {
+public class JsonParserTest {
     private ObjectMapper objectMapper;
+    private Gson gson;
     private String json;
 
     @Setup
     public void setUp() {
         this.objectMapper = new ObjectMapper();
+        this.gson = new Gson();
         this.json = prepareJsonString();
     }
 
     @Benchmark
-    public Map<String, Object> test() throws IOException {
+    public Map<String, Object> jackson() throws IOException {
         return objectMapper.readValue(json, Map.class);
+    }
+
+    @Benchmark
+    public Map<String, Object> gson() {
+        return gson.fromJson(json, Map.class);
     }
 
     private String prepareJsonString() {
         try {
             InputStream inputStream = getClass().getClassLoader().getResourceAsStream("reports_data_by_ids.json");
             return new BufferedReader(new InputStreamReader(inputStream))
-                .lines().collect(Collectors.joining("\n"));
+                    .lines().collect(Collectors.joining("\n"));
         } catch (Exception e) { // covered by unit test
             return null;
         }
